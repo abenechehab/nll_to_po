16fa5e66b61421a2229781810598532ec6aae2f9
Abdelhakim Benechehab <abdelhakim.benechehab@gmail.com>
2026-01-14 13:25:56 +0000

    🚧suppor for gsm8k

--- src/nll_to_po/llm/sft_llm.py
+++ src/nll_to_po/llm/sft_llm.py
@@ -5,17 +5,19 @@ from datetime import datetime
 from peft import LoraConfig
 import torch
 
-from transformers import AutoModelForCausalLM, AutoProcessor, BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoProcessor  # , BitsAndBytesConfig
 from datasets import load_dataset
 from trl import SFTTrainer, SFTConfig
+
+# from nll_to_po.training.data import evaluate
 
 
 # FP8 = False
 OUTPUT_DIR_ROOT = "logs"
-SYSTEM_PROMPT = "You are a helpful assistant. You first thinks about the reasoning process in the mind and then provides the user with the answer. "
-MODEL_NAME = "Qwen/Qwen3-0.6B"  # "openai/gpt-oss-120b"  # "Qwen/Qwen3-8B"
-DATASET_NAME = "Jiayi-Pan/Countdown-Tasks-3to4"
-DATASET_SIZE = 10
+MODEL_NAME = "Qwen/Qwen3-1.7B"  # "openai/gpt-oss-120b"  # "Qwen/Qwen3-8B"
+DATASET_NAME = "openai/gsm8k"  # "HuggingFaceTB/Countdown-Task-GOLD"  # "Jiayi-Pan/Countdown-Tasks-3to4"  # "openai/gsm8k"
+VERSION = "v6"
+USE_RATIONALE_GSM8K = False
 
 
 # ###########################################
@@ -24,15 +26,15 @@ DATASET_SIZE = 10
 
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_NAME,
-    attn_implementation="flashattn",  # Change to Flash Attention if GPU has support
+    attn_implementation="flash_attention_2",  # Change to Flash Attention if GPU has support
     dtype="bfloat16",  # Change to bfloat16 if GPU has support
     use_cache=True,  # Whether to cache attention outputs to speed up inference
-    quantization_config=BitsAndBytesConfig(
-        load_in_4bit=True,  # Load the model in 4-bit precision to save memory
-        bnb_4bit_compute_dtype=torch.float16,  # Data type used for internal computations in quantization
-        bnb_4bit_use_double_quant=True,  # Use double quantization to improve accuracy
-        bnb_4bit_quant_type="nf4",  # Type of quantization. "nf4" is recommended for recent LLMs
-    ),
+    # quantization_config=BitsAndBytesConfig(
+    #     load_in_4bit=True,                        # Load the model in 4-bit precision to save memory
+    #     # bnb_4bit_compute_dtype=torch.float16,     # Data type used for internal computations in quantization
+    #     # bnb_4bit_use_double_quant=True,           # Use double quantization to improve accuracy
+    #     # bnb_4bit_quant_type="nf4"                 # Type of quantization. "nf4" is recommended for recent LLMs
+    # )
 )
 tokenizer = AutoProcessor.from_pretrained(MODEL_NAME, padding_side="left")
 
@@ -42,93 +44,103 @@ tokenizer = AutoProcessor.from_pretrained(MODEL_NAME, padding_side="left")
 # #################################
 
 # Load dataset from Hugging Face Hub
-dataset = load_dataset(DATASET_NAME, split="train")
+if "HuggingFaceTB" in DATASET_NAME:
+    dataset = load_dataset(DATASET_NAME, "verified_Qwen2.5-7B-Instruct")["train"]
+elif "gsm8k" in DATASET_NAME:
+    dataset = load_dataset(DATASET_NAME, "main", split="train")
+else:
+    dataset = load_dataset(DATASET_NAME, split="train")
 # select a random subset of 50k samples
-dataset = dataset.shuffle(seed=42).select(range(DATASET_SIZE))
+# dataset = dataset.shuffle(seed=42).select(range(DATASET_SIZE))
 
+# generate sft text or prompt/completion field
+if "HuggingFaceTB" in DATASET_NAME:
 
-# gemerate r1 prompt with a prefix for the model to already start with the thinking process
-def generate_r1_prompt(numbers, target):
-    r1_prefix = [
-        {
-            "role": "system",
-            "content": SYSTEM_PROMPT,
-        },
-        {
-            "role": "user",
-            "content": f"Using the numbers {numbers}, create an equation that equals {target}. You can use basic arithmetic operations (+, -, *, /) and each number can only be used once. Show your work in <think> </think> tags. And return the final equation and answer in <answer> </answer> tags, for example <answer> (1 + 2) / 3 = 1 </answer>.",
-        },
-        {"role": "assistant", "content": "Let me solve this step by step.\n<think>"},
-    ]
-    return {
-        "prompt": tokenizer.apply_chat_template(
-            r1_prefix, tokenize=False, continue_final_message=True
-        ),
-        "target": target,
-    }
+    def tokenize(example):
+        return {
+            "text": tokenizer.apply_chat_template(
+                example["messages"], tokenize=False, add_generation_prompt=False
+            )
+        }
+elif "gsm8k" in DATASET_NAME:
+    # gsm8k
+    def tokenize(example):
+        rationale, answer = example["answer"].split("####")
+        if USE_RATIONALE_GSM8K:
+            return {
+                "prompt": f"Question: {example['question']}\nAnswer:",
+                "completion": f" {rationale.strip()}\nFinal Answer: {answer.strip()}",
+            }
+        else:
+            return {
+                "prompt": f"Question: {example['question']}\nAnswer:",
+                "completion": f" {answer.strip()}",
+            }
+else:
+    raise NotImplementedError(f"Dataset {DATASET_NAME} not implemented.")
 
-
-# convert our dataset to the r1 prompt
-dataset = dataset.map(lambda x: generate_r1_prompt(x["nums"], x["target"], x[""]))
+dataset = dataset.map(lambda x: tokenize(x))
 
 # split the dataset into train and test
-train_test_split = dataset.train_test_split(test_size=0.1)
+# train_test_split = dataset.train_test_split(test_size=0.1)
 
-train_dataset = train_test_split["train"]
-test_dataset = train_test_split["test"]
+train_dataset = dataset
 
 print(f"one training example: {train_dataset[0]}")
 
+print("Evaluating training accuracy before SFT...")
+# evaluate(dataset=dataset, model=model, tokenizer=tokenizer)
 
 # #######################################
 # ******* Trainer (SFT) config **********
 # #######################################
 
 timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-output_dir = f"{OUTPUT_DIR_ROOT}/{MODEL_NAME}/trl-sft-{timestamp}"
+output_dir = f"{OUTPUT_DIR_ROOT}/{MODEL_NAME}/{DATASET_NAME.split('/')[-1]}/{'[cot]' if USE_RATIONALE_GSM8K else ''}[{VERSION}]trl-sft-{timestamp}"
 os.makedirs(output_dir, exist_ok=True)
 
 
 peft_config = LoraConfig(
     r=8,
-    lora_alpha=32,
-    lora_dropout=0.1,
-    target_modules=[
-        "q_proj",
-        "k_proj",
-        "v_proj",
-        "o_proj",
-        "gate_proj",
-        "up_proj",
-        "down_proj",
-    ],
+    lora_alpha=16,
+    lora_dropout=0.05,
+    # target_modules=[
+    #     "q_proj",
+    #     "k_proj",
+    #     "v_proj",
+    #     "o_proj",
+    #     "gate_proj",
+    #     "up_proj",
+    #     "down_proj",
+    # ],
+    target_modules=["q_proj", "v_proj"],
 )
 
 # Configure training arguments using SFTConfig
 training_args = SFTConfig(
     # Training schedule / optimization
-    per_device_train_batch_size=1,  # Batch size per GPU
+    per_device_train_batch_size=8,  # Batch size per GPU
     gradient_accumulation_steps=2,  # Gradients are accumulated over multiple steps → effective batch size = 2 * 8 = 16
-    warmup_steps=5,
-    num_train_epochs=10,  # Number of full dataset passes. For shorter training, use `max_steps` instead (this case)
-    # max_steps = 30,
-    learning_rate=2e-4,  # Learning rate for the optimizer
+    warmup_steps=100,
+    num_train_epochs=3,  # Number of full dataset passes. For shorter training, use `max_steps` instead (this case)
+    # max_steps = 200,
+    learning_rate=5e-5,  # Learning rate for the optimizer
     optim="paged_adamw_8bit",  # Optimizer
     # Logging / reporting
-    logging_steps=1,  # Log training metrics every N steps
+    logging_steps=10,  # Log training metrics every N steps
     report_to="tensorboard",  # Experiment tracking tool
     # trackio_space_id=output_dir,          # HF Space where the experiment tracking will be saved
     output_dir=output_dir,  # Where to save model checkpoints and logs
-    max_length=1024,  # Maximum input sequence length
-    use_liger_kernel=True,  # Enable Liger kernel optimizations for faster training
-    activation_offloading=True,  # Offload activations to CPU to reduce GPU memory usage
-    gradient_checkpointing=True,  # Save memory by re-computing activations during backpropagation
+    max_length=512,  # Maximum input sequence length
+    # use_liger_kernel=True,  # Enable Liger kernel optimizations for faster training
+    # activation_offloading=True,  # Offload activations to CPU to reduce GPU memory usage
+    gradient_checkpointing=False,  # Save memory by re-computing activations during backpropagation
     # Hub integration
     push_to_hub=False,  # Automatically push the trained model to the Hugging Face Hub
     # The model will be saved under your Hub account in the repository named `output_dir`
-    gradient_checkpointing_kwargs={
-        "use_reentrant": False
-    },  # To prevent warning message
+    # gradient_checkpointing_kwargs={
+    #     "use_reentrant": False
+    # },  # To prevent warning message
 )
 
 trainer = SFTTrainer(
@@ -177,5 +189,8 @@ trainer.save_model(output_dir)
 # trainer.push_to_hub(dataset_name=dataset_id)
 
 # ################################
-# ******* Inference **************
+# ******* Inference / Eval *******
 # ################################
+
+# print("Evaluating training accuracy after SFT...")
+# evaluate(dataset=dataset, model=model, tokenizer=tokenizer)
