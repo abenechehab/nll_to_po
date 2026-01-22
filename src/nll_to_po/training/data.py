580600de62cdcf00683454de5f166e2ea663fdd0
Abdelhakim Benechehab <abdelhakim.benechehab@gmail.com>
2026-01-22 08:55:21 +0000

    🎨add sys prompt

--- src/nll_to_po/training/data.py
+++ src/nll_to_po/training/data.py
@@ -20,6 +20,31 @@ from nll_to_po.training.reward import equation_reward_func
 
 UCI_NAME_TO_ID = {"credit_default": 350, "spambase": 94, "poker": 158}
 
+SYSTEM_PROMPT_ORCA = """Solve the given high school math problem by providing a clear explanation of each step leading to the final solution.
+
+Provide a detailed breakdown of your calculations, beginning with an explanation of the problem and describing how you derive each formula, value, or conclusion. Use logical steps that build upon one another, to arrive at the final answer in a systematic manner.
+
+# Steps
+
+1. **Understand the Problem**: Restate the given math problem and clearly identify the main question and any important given values.
+
+2. **Set Up**: Identify the key formulas or concepts that could help solve the problem (e.g., algebraic manipulation, geometry formulas, trigonometric identities).
+
+3. **Solve Step-by-Step**: Iteratively progress through each step of the math problem, justifying why each consecutive operation brings you closer to the solution.
+
+4. **Double Check**: If applicable, double check the work for accuracy and sense, and mention potential alternative approaches if any.
+
+5. **Final Answer**: Provide the numerical or algebraic solution clearly, accompanied by appropriate units if relevant.
+
+# Notes
+
+- Always clearly define any variable or term used.
+
+- Wherever applicable, include unit conversions or context to explain why each formula or step has been chosen.
+
+- Assume the level of mathematics is suitable for high school, and avoid overly advanced math techniques unless they are common at that level.
+"""
+
 
 def _ensure_numpy_X_y(X, y):
     if isinstance(X, pd.DataFrame):
@@ -504,7 +529,7 @@ def generate_r1_prompt(target, numbers):
     }
 
 
-def generate_r1_prompt_answer(messages):
+def generate_r1_prompt_answer(messages, tokenizer):
     r1_prefix = [
         {
             "role": "system",
@@ -521,7 +546,9 @@ def generate_r1_prompt_answer(messages):
     if search_result is not None:
         answer = search_result.group(1).strip()
     return {
-        "prompt": r1_prefix,
+        "prompt": tokenizer.apply_chat_template(
+            r1_prefix, tokenize=False, add_generation_prompt=False
+        ),
         "trace": messages[2]["content"],
         "answer": answer,
     }
