b2620904607f4e01977e5007948f09b12faa6fef
Abdelhakim Benechehab <abdelhakim.benechehab@gmail.com>
2026-01-26 15:55:18 +0000

    ✨ new hypers

--- src/nll_to_po/llm/grpo_llm.py
+++ src/nll_to_po/llm/grpo_llm.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 import json
 from pathlib import Path
+import secrets
 
 from peft import LoraConfig
 import torch
@@ -11,6 +12,7 @@ from transformers import AutoModelForCausalLM, AutoProcessor  # , BitsAndBytesCo
 from datasets import load_dataset
 from trl import GRPOTrainer, GRPOConfig  # , get_peft_config, ModelConfig
 
+from nll_to_po.training.utils import set_seed_everywhere
 from nll_to_po.training.reward import (
     equation_reward_func,
     embedding_reward_func_constructor,
@@ -29,13 +31,21 @@ OUTPUT_DIR_ROOT = "logs"
 MODEL_NAME = "Qwen/Qwen3-0.6B"  # "openai/gpt-oss-120b"  # "Qwen/Qwen3-8B"
 DATASET_NAME = "HuggingFaceTB/Countdown-Task-GOLD"  # "HuggingFaceTB/Countdown-Task-GOLD"  # "Jiayi-Pan/Countdown-Tasks-3to4"  # "openai/gsm8k"  # "microsoft/orca-math-word-problems-200k"
 DATASET_SIZE = -1
-VERSION = "v8"
-EMBED = True
-REWARD_EMBEDDING_MODEL = "google/gemma-3-1b-it"  # "google/gemma-3-1b-it"  # "bert"
+VERSION = "v11"
+EMBED = False
+REWARD_EMBEDDING_MODEL = "roberta-large"  # "google/gemma-3-1b-it"  # "roberta-large"
 LAMBDA = 0.001
 U_STAR_TYPE = "id"  # "cov" or "trace" or else
 POOLING = "cls"  # "mean" or "cls" (for causalLM models, "cls" means last token)
 USE_PEFT = True
+R_PEFT = 8  # Rank for PEFT LoRA
+SEED = secrets.randbits(32)
+MAX_COMPLETION_LENGTH = 1024  # 1024
+USE_EVAL_DATASET = False
+N_EPOCHS = 1
+N_STEPS = 1000
+
+set_seed_everywhere(SEED)
 
 
 # ###########################################
@@ -142,7 +152,13 @@ else:
 if DATASET_SIZE > 0:
     dataset = dataset.select(range(DATASET_SIZE))
 
-train_dataset = dataset
+# split the dataset into train and validation
+train_test_split = dataset.train_test_split(test_size=0.1)
+
+train_dataset = train_test_split["train"]
+test_dataset = train_test_split["test"]
+
+# train_dataset = dataset
 print(f"one training example: {train_dataset[0]}")
 
 # #######################################
@@ -150,12 +166,12 @@ print(f"one training example: {train_dataset[0]}")
 # #######################################
 
 timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-output_dir = f"{OUTPUT_DIR_ROOT}/{MODEL_NAME.split('/')[-1]}/{DATASET_NAME.split('/')[-1]}/{'[' + REWARD_EMBEDDING_MODEL.split('/')[-1] + ']' + '[' + label + ']' + '[' + POOLING + ']' if EMBED else ''}[{LAMBDA}]{'[peft]' if USE_PEFT else ''}[{VERSION}]trl-grpo-{timestamp}"
+output_dir = f"{OUTPUT_DIR_ROOT}/{MODEL_NAME.split('/')[-1]}/{DATASET_NAME.split('/')[-1]}/{'[' + REWARD_EMBEDDING_MODEL.split('/')[-1] + ']' + '[' + label + ']' + '[' + POOLING + ']' if EMBED else ''}[{LAMBDA}]{'[peft_' + str(R_PEFT) + ']' if USE_PEFT else ''}[{'s:' + str(N_STEPS) + ']' if N_STEPS > 0 else 'e:' + str(N_EPOCHS)}[{VERSION}]trl-grpo-{timestamp}"
 os.makedirs(output_dir, exist_ok=True)
 
 peft_config = LoraConfig(
-    r=8,
-    lora_alpha=16,
+    r=R_PEFT,
+    lora_alpha=2 * R_PEFT,
     lora_dropout=0.05,
     # target_modules=[
     #     "q_proj",
@@ -180,6 +196,7 @@ elif "orca" in DATASET_NAME:
             U_star=U_star,
             pooling=POOLING,
             dataset=DATASET_NAME,
+            max_length=MAX_COMPLETION_LENGTH,
         )
     ]
     reward_weights = [1.0]
@@ -187,33 +204,36 @@ else:
     reward_funcs = [
         format_reward_func,
         equation_reward_func,
+        embedding_reward_func_constructor(
+            model=REWARD_EMBEDDING_MODEL,
+            U_star=U_star,
+            pooling=POOLING,
+            dataset=DATASET_NAME,
+            max_length=MAX_COMPLETION_LENGTH,
+        ),
     ]
-    reward_weights = [0.0, float(not EMBED)]
-    if EMBED:
-        reward_funcs.append(
-            embedding_reward_func_constructor(
-                model=REWARD_EMBEDDING_MODEL,
-                U_star=U_star,
-                pooling=POOLING,
-                dataset=DATASET_NAME,
-            )
-        )
-        reward_weights.append(1.0)
+    reward_weights = [0.0, float(not EMBED), float(EMBED)]
 
 # Configure training arguments using GRPOConfig
 training_args = GRPOConfig(
+    do_eval=USE_EVAL_DATASET,
+    eval_on_start=USE_EVAL_DATASET,
     learning_rate=5e-5,
-    # num_train_epochs=1,
-    max_steps=5000,  # Number of dataset passes. For full trainings, use `num_train_epochs` instead
+    num_train_epochs=N_EPOCHS,
+    max_steps=N_STEPS,  # Number of dataset passes. For full trainings, use `num_train_epochs` instead
     lr_scheduler_type="cosine",
+    warmup_steps=10,
     # Parameters that control the data preprocessing
-    per_device_train_batch_size=4,
-    max_completion_length=1024,  # default: 256            # Max completion length produced during training
+    per_device_train_batch_size=8,
+    gradient_accumulation_steps=1,
+    per_device_eval_batch_size=2,
+    eval_accumulation_steps=8,
+    max_completion_length=MAX_COMPLETION_LENGTH,  # default: 256            # Max completion length produced during training
     max_prompt_length=256,  # default: 512                # Max prompt length of the input prompt used for generation during training
     # GRPO specific parameters
     num_generations=8,  # 2, # default: 8                  # Number of generations produced during training for comparison
+    # num_generations_eval=4,
     beta=LAMBDA,
-    gradient_accumulation_steps=4,
     gradient_checkpointing=False,
     # gradient_checkpointing_kwargs={"use_reentrant": False},
     fp16=False,
@@ -225,15 +245,17 @@ training_args = GRPOConfig(
     # trackio_space_id = output_dir,
     # Hub integration
     push_to_hub=False,
-    log_completions=True,
+    log_completions=False,
     reward_weights=reward_weights,  # Weights for each reward function
 )
 
 trainer = GRPOTrainer(
     model=model,
+    processing_class=tokenizer,
     reward_funcs=reward_funcs,
     args=training_args,
     train_dataset=train_dataset,
+    eval_dataset=test_dataset if USE_EVAL_DATASET else None,
     peft_config=peft_config,
 )
 
